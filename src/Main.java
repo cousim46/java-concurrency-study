d6b5f025b131318c77903d473b8bc82bff11e9d9
jhw1015 <jhw1015@bank-mall.co.kr>
2024-03-20 15:12:49 +0900

    프로젝트 생성

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+/**
+ * packageName    : 
+ * fileName       : ${NAME}
+ * author         : hoewoonjeong
+ * date           : 3/20/24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 3/20/24        hoewoonjeong               최초 생성
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
